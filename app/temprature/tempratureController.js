8b544d1b32c107e7c4342141179ff3a498419b4e
parthiv1993 <parthiv1993.pp@gmail.com>
2018-02-28 13:50:28 +0530

    Update tempratureController.js

--- app/temprature/tempratureController.js
+++ app/temprature/tempratureController.js
@@ -12,8 +12,9 @@ module.exports = (function(){
         });
     }
 
-    function _insertTempratureData(req, res) {
+    function _insertTempratureData(req, res) 
         var tempratureData = req.body;
+        console.log('temperature to insert',tempratureData);
         TempratureModel.insertTempratureData(tempratureData,function(){
             res.json({ok: 'ok'});
         },
