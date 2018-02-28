9b1e61e0fa90c5907dc30ace970839426029d237
parthiv1993 <parthiv1993.pp@gmail.com>
2018-02-28 13:52:43 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var bodyParser = require('body-parser');
+var morgan = require('morgan')
 
 var app = express()
 
+app.use(morgan('combined'))
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
